b969cbba5e8b1b3489acddd35b9fe21e894683e4
Kirill <budanovkirilldmitrievich@gmail.com>
2021-12-16 22:02:01 +0300

    dz

---
+++ task_2.py
@@ -0,0 +1,10 @@
+str_item = [b'class', b'function', b'method']
+
+for i in str_item:
+    print(type(i), i, len(i))
+
+"""
+<class 'bytes'> b'class' 5
+<class 'bytes'> b'function' 8
+<class 'bytes'> b'method' 6
+"""
